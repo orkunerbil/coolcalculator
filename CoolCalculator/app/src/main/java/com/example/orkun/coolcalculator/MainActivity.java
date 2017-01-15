e01a95b2157fd33727bbd36d672cfc7bdede6d28
ORKUN ERBIL <orkun_erbil_1992@hotmail.com>
2017-01-16 00:43:49 +0300

    Basit Hesap Makinesi Uygulaması
    Tasarım

---
+++ CoolCalculator/app/src/main/java/com/example/orkun/coolcalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.orkun.coolcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
